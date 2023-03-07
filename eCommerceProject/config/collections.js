8b7b6e490179a0038e2a7671cd1677f5847f111b
Athul Sabu <116870115+Athulolikkal@users.noreply.github.com>
2023-03-07 12:33:29 +0530

    project deleted

--- eCommerceProject/config/collections.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-    PRODUCT_COLLECTIONS:'product',
-    USER_COLLECTIONS:'user',
-    ADMIN_COLLECTIONS:'admin',
-    CATEGORY_COLLECTIONS:'category',
-    CART_COLLECTIONS:'cart',
-    ORDER_COLLECTIONS:'order',
-    COUPON_COLLECTION:'coupon'
-}
--- eCommerceProject/utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2
-
-cloudinary.config({
-    cloud_name:"dvwg28xxv",
-    api_key:"445429192933544",
-    api_secret: "FMjSEzA4JpCfVwl40Hv9wtdYevo"
-})
-
-module.exports = cloudinary
